ad90e4c23980e57e511553215d9d246875313604
Samfeolu <samfeolu@gmail.com>
2019-02-27 00:58:32 +0100

    Solved Up to Product Post Issue

--- script.js
+++ script.js
@@ -44,14 +44,50 @@ $(document).ready(
                     window.location = 'index.html'
                     document.cookie = `staffId=${res[0].id}, Firstname=${res[0].firstname}, Lastname=${res[0].lastname}`
                 }
-                    //else{alert('There is a problem')}
-                
-               //alert(`Our Staff FirstName is ${res[0].firstname} Our Staff Lastname is ${res[0].lastname}  `);
-               
-            })
-        
+            })   
     }
 }
     )
+)
 
+//  Product Creation Validation Script
+
+$(document).ready(
+    $("#addProduct").submit(
+        function(e){
+            e.preventDefault();
+            document.getElementById('load').setAttribute('id','show')
+            $.ajax({
+                method:"GET",
+                url: `http://localhost:3000/product?name=${$("#addProductName").val()}`
+            }).done(
+
+                function(res){
+                    if(res.length === 0){
+                        $.ajax({
+                            method:"POST",
+                            url: "http://localhost:3000/product",
+                            data: {
+                                name:`${$("#addProductName").val()}`,
+                                quantity:`${$("#addQuantity").val()}`,
+                                costPrice:`${$("#addCostPrice").val()}`,
+                                sellingPrice: `${$("#addSellingPrice").val()}`
+                            }
+                        })
+                        .done(
+                            //console.log('Your Product Was Successfully Added')
+                            window.location='productOperations.html'
+                        )
+                        .fail(alert('Sorry Something Went Wrong'))
+                    }
+                    else{
+                        alert('Sorry Product Exist')
+                    }
+                }
+            ).fail(
+                alert('Error Validating From Server')
+            )
+            
+        }
     )
+)
