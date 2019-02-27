f6b4ee8e7fa7f52b62af60d06a1792a54c60d725
Samfeolu <samfeolu@gmail.com>
2019-02-27 13:03:05 +0100

    Added Staff and Admin Authentication, Checked Deadlinks and Fixed Visible ones, Added Comments to Scripts sections, Created files(transaction, staffpanel, logout,) Added all files with undercore from partner

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(
 }
     )
 
-    // Login Validation Script
+//  Staff Login Validation Script
     
 )
 
@@ -40,9 +40,14 @@ $(document).ready(
                     $("#emailHelp").val('')
                 }
                 else{
-                    console.log(res)
-                    window.location = 'index.html'
-                    document.cookie = `staffId=${res[0].id}, Firstname=${res[0].firstname}, Lastname=${res[0].lastname}`
+                    alert(`Welcome ${res[0].firstname}`)
+                    window.location = 'staffPanel.html';
+                    let firstName = res[0].firstname;
+                    let lastName = res[0].lastname;
+                    let userId = res[0].id;
+                    window.localStorage.setItem('staffId',`${userId}`)
+                    window.localStorage.setItem('name',`${firstName} ${lastName}`)
+                    //document.cookie = `staffId=${res[0].id}, Firstname=${res[0].firstname}, Lastname=${res[0].lastname}`
                 }
             })   
     }
@@ -148,3 +153,205 @@ $(document).ready(
         }
     )
 )
+
+//  Get a list of all product
+$(document).on('load', 
+    function(){
+        $.ajax({
+            method:"GET",
+            url: `http://localhost:3000/product`
+        })
+        .done(function(res){
+            alert(res);
+            let select = document.createElement('select')
+            let doc = getElementById('selectProduct');
+            doc.appendChild(select)
+            res.forEach(function (product){
+                alert(product.name);
+                let option = document.createElement('option');
+                option.innerText = product.name
+                select.appendChild(option)
+            })
+            let divInDoc = getElementById('selectProduct');
+            divInDoc.appendChild('')
+        })
+        
+    }
+)
+
+//////      Addition Of Staff  
+$(document).ready(function(){
+    $( "#addStaff" ).submit(function( event ) {
+      event.preventDefault();  
+      alert('submitted');
+      var firstName=$("#firstname").val();
+      var lastName=$("#lastname").val();
+      var Gender=$("#gender").val();
+      var emailAddress=$("#email").val();
+      var phoneNumber=$("#phone").val();
+      var defaultPassword=$("#defaultPassword").val();
+      var confirmDefault=$("#confirmDefault").val();
+      var obj={
+          firstname:firstName,
+          lastname:lastName,
+          gender:Gender,
+          email:emailAddress,
+          phone:phoneNumber,
+          password:defaultPassword
+      }
+      //console.log(obj);
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/staff/",
+            data:obj 
+          })
+            .done(function( msg ) {
+              //console.log(msg);
+              alert( "Staff successfully added" );
+              location.reload();
+            });
+        
+      });
+  });
+
+//  Admin Login Validation
+$(document).ready(
+    $("#adminLogin").submit(function(event){
+        event.preventDefault();
+        
+        if ( $("#adminId").val() && $("#password").val()) {
+        
+            $.ajax({
+              method: "GET",
+              url: `http://localhost:3000/admin?id=${$("#adminId").val()}&password=${$("#password").val()}`
+              
+            })
+            .done(function(res){
+                if(res.length===0){
+                    alert('Invalid Login');
+                }
+                else{
+                    alert('Welcome Admin');
+                    //console.log(res)
+                    window.localStorage.clear()
+                    window.localStorage.setItem('adminId', `${res[0].id}`)
+
+                    window.location="admin.html";
+                }
+                    //else{alert('There is a problem')}
+                
+               //alert(`Our Staff FirstName is ${res[0].firstname} Our Staff Lastname is ${res[0].lastname}  `);
+               
+            })
+        
+    }
+}
+    )
+
+    );
+
+//  Addition Of Customer Script
+$(document).ready(function(){
+        $( "#addCustomer" ).submit(function( event ) {
+          event.preventDefault();  
+          //alert('submitted');
+          var firstName=$("#firstname").val();
+          var lastName=$("#lastname").val();
+          var Gender=$("#gender").val();
+          var emailAddress=$("#email").val();
+          var phoneNumber=$("#phone").val();
+          var staffId = window.localStorage.getItem('staffId') ;
+          console.log(current_cookie[1]); 
+          var obj={
+              firstname:firstName,
+              lastname:lastName,
+              gender:Gender,
+              email:emailAddress,
+              phone:phoneNumber,
+              addedBy:staffId
+                
+            }
+          //console.log(obj);
+            $.ajax({
+                method: "POST",
+                url: "http://localhost:3000/user/",
+                data:obj 
+              })
+                .done(function( msg ) {
+                  //console.log(msg);
+                  alert( "Staff successfully added" );
+                  location.reload();
+                });
+            
+          });
+      });
+ 
+//   Adding New Staff To DataBase 
+$(document).ready(function(){
+    $( "#addStaff" ).submit(function( event ) {
+      event.preventDefault();  
+      //alert('submitted');
+      var firstName=$("#firstname").val();
+      var lastName=$("#lastname").val();
+      var Gender=$("#gender").val();
+      var emailAddress=$("#email").val();
+      var phoneNumber=$("#phone").val();
+      var defaultPassword=$("#defaultPassword").val();
+      var confirmDefault=$("#confirmDefault").val();
+      var obj={
+          firstname:firstName,
+          lastname:lastName,
+          gender:Gender,
+          email:emailAddress,
+          phone:phoneNumber,
+          password:defaultPassword
+      }
+      //console.log(obj);
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/staff/",
+            data:obj 
+          })
+            .done(function( msg ) {
+              //console.log(msg);
+              alert( "Staff successfully added" );
+              location.reload();
+            });
+        
+      });
+  });
+  
+  //    Phone Number Validation For Creating Staff Account
+  function validate_phone_number(){
+    var phone_length=document.getElementById('phone').value.length;
+    if (phone_length ===11){
+        document.getElementById('submit_button').disabled=false;
+        document.getElementById('confirm_phone_number').style.backgroundColor="green";
+        document.getElementById('confirm_phone_number').innerText="Valid phone number";
+    }
+    else{
+        document.getElementById('submit_button').disabled=true;
+        document.getElementById('confirm_phone_number').style.backgroundColor="red";
+    
+        document.getElementById('confirm_phone_number').innerText="Invalid Phone nUmber";
+    }
+    
+    
+}
+
+//Password Matching Validation for Creating Staff Account
+function confirm_pass(){
+var password=document.getElementById('defaultPassword').value;
+var confirm=document.getElementById('confirmDefault').value;
+if (password===confirm){
+    document.getElementById('submit_button').disabled=false;
+    document.getElementById('confirm_pass_small').style.backgroundColor="green";
+    document.getElementById('confirm_pass_small').innerText="Confirm password and password matches";
+}
+else{
+    document.getElementById('submit_button').disabled=true;
+    document.getElementById('confirm_pass_small').style.backgroundColor="red";
+
+    document.getElementById('confirm_pass_small').innerText="Confirm password and password mismatch";
+}
+}
